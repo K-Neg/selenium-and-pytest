185fad525b3fac54d90201018d4b7874d55bd482
K-Neg <35573703+K-Neg@users.noreply.github.com>
2021-02-16 11:36:42 -0300

    add remove and get

--- water-galon-API/app/source/routes/gallon.py
+++ water-galon-API/app/source/routes/gallon.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from source.schemas.gallon import GallonBase, GallonUpdate
-from source.crud.gallons import insert_gallon, retrieve_all_galllons
+from source.crud.gallons import insert_gallon, retrieve_all_galllons, retrieve_gallon, remove_gallon
 from source.database.database import SessionLocal, engine
 from sqlalchemy.orm import Session
 
@@ -15,14 +15,14 @@ def get_db():
 
 
 @gallon_routes.get("/")
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    gallons = retrieve_all_galllons(db=db, skip=skip, limit=limit)
+def get_all(db: Session = Depends(get_db)):
+    gallons = retrieve_all_galllons(db=db)
     return gallons
 
-
 @gallon_routes.get("/{code}")
-async def get_one(code: int):
-    return 1
+async def get_one(code: int,db: Session = Depends(get_db)):
+    gallon = retrieve_gallon(code=code,db=db)
+    return gallon
 
 @gallon_routes.post("/")
 async def create_gallon(gallon: GallonBase, db: Session = Depends(get_db)):
@@ -34,5 +34,6 @@ async def update_gallon(code: int, gallon: GallonUpdate):
     return gallon
 
 @gallon_routes.delete("/{code}")
-async def delete_gallon(code: int):
+async def delete_gallon(code: int, db: Session = Depends(get_db)):
+    remove_gallon(code=code, db=db)
     pass
